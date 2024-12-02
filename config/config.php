e5b4b25cb11a9bd22c5c2ea3c2cfa99aefa0c42c
AnhHQ98 <haquanganh12a1@gmail.com>
2024-12-02 17:00:59 +0700

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php 
+// DEFINE('INDEX_URL','http://localhost:8888/WBH_MVC-master/');
+DEFINE('INDEX_URL','http://localhost/18/2024_11_26_T3_webBanDongHo/WBH_MVC-master/');
+
+// DEFINE('DB_HOST','localhost:8889');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_USERNAME','root');
+// DEFINE('DB_PASSWORD','root');
+DEFINE('DB_PASSWORD','');
+DEFINE('DB_NAME','241126t3');
+
+DEFINE('CONTROLLER_DEFAULT','index');
+DEFINE('ACTION_DEFAULT','index');
+?>
